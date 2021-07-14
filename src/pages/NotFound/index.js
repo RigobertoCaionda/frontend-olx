d6490db258b1b9eb0a2fbb036355b2745bc0202d
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-07-14 20:22:12 +0100

    Adding things to the page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+const Page = ()=>{
+	return (
+			<div>
+				<h1>Pagina nao encontrada!</h1>
+				<Link to="/">Voltar para a Home</Link>
+			</div>
+		);
+}
+export default Page;
